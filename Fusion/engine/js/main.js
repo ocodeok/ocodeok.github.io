5672ff5abd5e4f18429338091084f43f77f4b21c
ocodeok <54496436+ocodeok@users.noreply.github.com>
2020-03-14 18:31:08 +0300

    add slide

---
+++ Fusion/engine/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.slider-main').slick({
+		arrows:false,
+		dots:true,
+		slidesToShow:1,
+		autoplay:false,
+		speed:3000,
+		autoplaySpeed:1500,
+		waitForAnimate : false
+	});
+});
